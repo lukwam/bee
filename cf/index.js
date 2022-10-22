d6fa427b720c5d1cc24cb3ca76e8261e0cf5f6a3
Lukas Karlsson <admin@lukwam.dev>
2022-10-22 04:02:45 +0000

    First commit.

---
+++ cf/index.js
@@ -0,0 +1,193 @@
+const functions = require('@google-cloud/functions-framework');
+const fetch = require('node-fetch');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+
+function getDate() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = yyyy + '/' + mm + '/' + dd;
+    return today;
+}
+
+function getUrl() {
+    var date = getDate();
+    var url = "https://www.nytimes.com/" + date + "/crosswords/spelling-bee-forum.html"
+    return url;
+}
+
+function parseContent(articleBody) {
+    console.log(articleBody);
+    var content = articleBody.childNodes[3].childNodes[0].childNodes[0].childNodes[7];
+    // var content = articleBody.getElementsByClass("content");
+    // console.log(content);
+    // get letters
+    var middle = content.childNodes[3].childNodes[1].innerHTML.trim();
+    var rest = content.childNodes[3].childNodes[3].innerHTML.trim();
+    var letters = (middle + rest).replace(/\s/g, '');
+    console.log("Letters: " + letters);
+
+    // get status
+    var stats = content.childNodes[5].innerHTML.trim().split(", ");
+
+    // get words
+    var words = 0;
+    for (stat of stats) {
+        if (stat.startsWith("WORDS: ")) {
+            words = parseInt(stat.split(": ")[1])
+        }
+    }
+    console.log("Words: " + words);
+
+    // get points
+    var points = 0;
+    for (stat of stats) {
+        if (stat.startsWith("POINTS: ")) {
+            points = parseInt(stat.split(": ")[1])
+        }
+    }
+    console.log("Points: " + points);
+
+    // get pangrams
+    var pangrams = 0;
+    for (stat of stats) {
+        if (stat.startsWith("PANGRAMS: ")) {
+            pangrams = parseInt(stat.split(": ")[1])
+        }
+    }
+    console.log("Pangrams: " + pangrams);
+
+    // check for bingo
+    var bingo = stats.includes("BINGO") ? true : false
+    console.log("Bingo: " + bingo);
+
+    // get the table
+    var tableRows = content.childNodes[7].childNodes[1].childNodes;
+    var header = false;
+    var data = {};
+    var lengths = [];
+    for (var row of tableRows) {
+        if (row.nodeName == "TR") {
+            if (!header) {
+                for (var h of row.childNodes) {
+                    if (h.nodeName == "TD") {
+                        let length = h.childNodes[0].data.trim();
+                        if (length) {
+                            lengths.push(length);
+                        }
+                    }
+                }
+                header = true;
+            } else {
+                let letter = "";
+                let letterLengths = [];
+                for (var d of row.childNodes) {
+                    if (d.nodeName == "TD") {
+                        if (!letter) {
+                            letter = d.childNodes[0].innerHTML;
+                            if (!letter) {
+                                letter = d.innerHTML.replace("-", "").trim();
+                            }
+                        } else {
+                            let value = d.innerHTML.replace("-", "").trim();
+                            letterLengths.push(value);
+                        }
+                    }
+                }
+                // console.log(row);
+                console.log(letter + ": " + letterLengths);
+                data[letter] = letterLengths;
+            }
+
+        }
+    }
+
+    // get the two-letter list
+    var twoLetters = content.childNodes[11];
+    var twoString = "";
+    for (var entry of twoLetters.childNodes) {
+        if (entry.nodeName == "SPAN") {
+            twoString += entry.innerHTML.trim().split("\n")[0] + " ";
+        }
+    }
+    twoList = twoString.trim().split(" ");
+
+    var text = "Letters: " + letters + "<br>\n";
+    text += "Words: " + words + "<br>\n";
+    text += "Points: " + points + "<br>\n";
+    text += "Pangrams: " + pangrams + "<br>\n";
+    text += "Bingo: " + bingo + "<br>\n";
+    text += "Lengths: " + lengths + "<br>\n";
+    text += "Letter Counts:\n<br>\n";
+    for (const [k, v] of Object.entries(data)) {
+        text += k + ": " + v + "<br>\n";
+    }
+    text += "Two-Letter List: " + twoList;
+
+
+    output = {
+        "letters": letters,
+        "words": words,
+        "points": points,
+        "pangrams": pangrams,
+        "bingo": bingo,
+        "lengths": lengths,
+        "letter_counts": data,
+        "two_letter_list": twoList,
+    }
+    return JSON.stringify(output);
+
+}
+
+function get_request(url, callback, response){
+    var options = {
+        "headers": {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0",
+        },
+    }
+    fetch(url, options)
+        .then(res => res.text())
+        .then(text => callback(text, response));
+}
+
+function parseBody(body, res) {
+    var dom = new JSDOM(body);
+    // console.log(dom);
+    var document = dom.window.document;
+    // console.log(dom.window);
+
+    // var contents = document.getElementsByClass("content");
+    // console.log(contents);
+
+    var articleBody = document.getElementsByName("articleBody")[0];
+    var output = parseContent(articleBody);
+    // console.log(output);
+    res.send(output);
+}
+
+functions.http('spellingBeeHints', (req, res) => {
+    // get the url of today's spelling bee
+    var url = getUrl();
+    // console.log(url);
+
+    // get content via http
+    var dom = get_request(url, parseBody, res);
+    console.log(dom);
+
+    // get dom
+    // var dom = new JSDOM(body);
+    // console.log(dom.window.document);
+
+    // const response = await fetch('https://reddit.com/');
+	// const body = await response.text();
+	// console.log(body); // prints a chock full of HTML richness
+	// return body;
+
+    // var output = getContent();
+    // res.send(response);
+    // res.send("ok");
+    // res.send("ok");
+});
--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,5 @@
+var data;
+
 function httpGet(theUrl) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
@@ -5,150 +7,29 @@ function httpGet(theUrl) {
     return xmlHttp.responseText;
 }
 
-function getDate() {
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
-    today = yyyy + '/' + mm + '/' + dd;
-    return today;
+function checkStats() {
+    var textarea = document.getElementById("words");
+    var words = textarea.value.split(" ");
+    console.log(words);
+
 }
 
 function loadPage() {
-    var date = getDate();
-    document.title = "Spelling Bee Hints: " + date.replaceAll("/", "-");
-    var div = document.getElementById("content");
-    var hintsUrl = "https://www.nytimes.com/" + date + "/crosswords/spelling-bee-forum.html"
-    var hintsString = httpGet(hintsUrl);
-    var parser = new DOMParser();
-    var hintsHtml = parser.parseFromString(hintsString, "text/html");
-    var content = hintsHtml.getElementsByName("articleBody")[0].childNodes[3].childNodes[0].childNodes[0].childNodes[7];
-
-    // get letters
-    var middle = content.childNodes[3].childNodes[1].innerHTML.trim();
-    var rest = content.childNodes[3].childNodes[3].innerHTML.trim();
-    var letters = (middle + rest).replace(/\s/g, '');
-    console.log("Letters: " + letters);
-
-    // get status
-    var stats = content.childNodes[5].innerHTML.trim().split(", ");
-
-    // get words
-    var words = 0;
-    for (stat of stats) {
-        if (stat.startsWith("WORDS: ")) {
-            words = parseInt(stat.split(": ")[1])
-        }
-    }
-    console.log("Words: " + words);
-
-    // get points
-    var points = 0;
-    for (stat of stats) {
-        if (stat.startsWith("POINTS: ")) {
-            points = parseInt(stat.split(": ")[1])
-        }
-    }
-    console.log("Points: " + points);
-
-    // get pangrams
-    var pangrams = 0;
-    for (stat of stats) {
-        if (stat.startsWith("PANGRAMS: ")) {
-            pangrams = parseInt(stat.split(": ")[1])
-        }
-    }
-    console.log("Pangrams: " + pangrams);
-
-    // check for bingo
-    var bingo = stats.includes("BINGO") ? true : false
-    console.log("Bingo: " + bingo);
-
-    // get the table
-    var tableRows = content.childNodes[7].childNodes[1].childNodes;
-    var header = false;
-    var data = {};
-    var lengths = [];
-    for (var row of tableRows) {
-        if (row.nodeName == "TR") {
-            if (!header) {
-                for (var h of row.childNodes) {
-                    if (h.nodeName == "TD") {
-                        let length = h.childNodes[0].data.trim();
-                        if (length) {
-                            lengths.push(length);
-                        }
-                    }
-                }
-                header = true;
-            } else {
-                let letter = "";
-                let letterLengths = [];
-                for (var d of row.childNodes) {
-                    if (d.nodeName == "TD") {
-                        if (!letter) {
-                            letter = d.childNodes[0].innerHTML;
-                            if (!letter) {
-                                letter = d.innerHTML.replace("-", "").trim();
-                            }
-                        } else {
-                            let value = d.innerHTML.replace("-", "").trim();
-                            letterLengths.push(value);
-                        }
-                    }
-                }
-                // console.log(row);
-                console.log(letter + ": " + letterLengths);
-                data[letter] = letterLengths;
-            }
-
-        }
-    }
-
-    // get the two-letter list
-    var twoLetters = content.childNodes[11];
-    var twoString = "";
-    for (var entry of twoLetters.childNodes) {
-        if (entry.nodeName == "SPAN") {
-            twoString += entry.innerHTML.trim().split("\n")[0] + " ";
-        }
-    }
-    twoList = twoString.trim().split(" ");
-
-    // data["_"] = lengths;
-    console.log(lengths);
+    var url = "https://spelling-bee-hints-ogqo6aktjq-uc.a.run.app/";
+    var jsonString = httpGet(url);
+    data = JSON.parse(jsonString);
     console.log(data);
 
-    // console.log(stats);
-    // div.appendChild(content);
+    // var words = ['civic', 'connive', 'connived', 'convene', 'convened', 'convenience', 'convince', 'convinced', 'cove', 'coven', 'dive', 'dived', 'divide', 'divided', 'dividend', 'divine', 'divined', 'divvied', 'dove', 'envied', 'even', 'evened', 'evidence', 'evidenced', 'inconvenience', 'inconvenienced', 'invoice', 'invoiced', 'ivied', 'novice', 'oven', 'ovine', 'ovoid', 'vend', 'vended', 'video', 'videoed', 'vied', 'voice', 'voiced', 'voodoo']
+    // var string = "";
+    // words.forEach((word) => string += word + " ");
+    // console.log(string.trim());
+    // var bwords = data["words"];
+    // var left = bwords - words.length;
+    // console.log("Left: " + left);
 
-    var text = "Letters: " + letters + "<br>\n";
-    text += "Words: " + words + "<br>\n";
-    text += "Points: " + points + "<br>\n";
-    text += "Pangrams: " + pangrams + "<br>\n";
-    text += "Bingo: " + bingo + "<br>\n";
-    text += "Lengths: " + lengths + "<br>\n";
-    text += "Letter Counts:\n<br>\n";
-    for (const [k, v] of Object.entries(data)) {
-        text += k + ": " + v + "<br>\n";
-    }
-    text += "Two-Letter List: " + twoList;
-
-    textDiv = document.getElementById("text");
-    // textDiv.innerHTML = text;
-
-    output = {
-        "letters": letters,
-        "words": words,
-        "points": points,
-        "pangrams": pangrams,
-        "bingo": bingo,
-        "lengths": lengths,
-        "letter_counts": data,
-        "two_letter_list": twoList,
-    }
-    textDiv.innerHTML = "<pre>" + JSON.stringify(output, null, 2) + "</pre>";
-
-
+    // var output = "";
+    // output += left + " words left to find out of " + bwords + "<br>\n";
+    // var div = document.getElementById("output");
+    // div.innerHTML = output;
 }
-
